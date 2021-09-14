2f2f6dee707d08a602747249738d1b5e15c21913
Delfina Fenocchio <delfina.fenocchio@tunubi.com>
2021-09-14 17:51:57 -0300

    Initial Commit

---
+++ proyecto-x/proyecto_xApp.swift
@@ -0,0 +1,17 @@
+//
+//  proyecto_xApp.swift
+//  proyecto-x
+//
+//  Created by Delfina Fenocchio on 14/09/2021.
+//
+
+import SwiftUI
+
+@main
+struct proyecto_xApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
