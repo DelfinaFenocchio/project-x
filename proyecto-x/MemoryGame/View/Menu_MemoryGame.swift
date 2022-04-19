21846c6b2b96aeb8b7a14fd52af02a610a67e7d5
Alan Diaz <Alanfdiaz@gmail.com>
2022-04-19 18:21:05 -0300

    add scrollView and onChange

--- proyecto-x/MemoryGame/View/Menu_MemoryGame.swift
+++ proyecto-x/MemoryGame/View/Menu_MemoryGame.swift
@@ -13,79 +13,83 @@ struct MemoryGameMenu: View {
     @StateObject var state : MemoryGameState = MemoryGameState()
     
     var body: some View {
-        VStack {
-            Group{
-                Text("Memory Game")
-                
-                Spacer()
-                
-                Text("Elegí el modo de juego")
-                    .fontWeight(.bold)
-                    .font(.system(.title))
-                    .padding()
-                    .customTextStyle()
-                
-                Picker("Modo de Juego", selection: $state.gameModeSelected) {
-                    Text("\(GameModeMemoryGame.classicMultiPlayer.rawValue)").tag(GameModeMemoryGame.classicMultiPlayer)
-                    Text("\(GameModeMemoryGame.classicSinglePlayer.rawValue)").tag(GameModeMemoryGame.classicSinglePlayer)
-                    Text("\(GameModeMemoryGame.sequential.rawValue)").tag(GameModeMemoryGame.sequential)
-                }
-                    .pickerStyle(SegmentedPickerStyle())
-                    .padding()
-                
-                Spacer()
-
-                Text("Cantidad de cards")
-                    .fontWeight(.bold)
-                    .font(.system(.title))
-                    .padding()
-                    .customTextStyle()
-                
-                Picker("Cards amount", selection: $state.cardsAmountSelected) {
-                    ForEach((1...CardAmountOptions.count), id: \.self) {index in
-                        let cardAmount = CardAmountOptions[state.gameModeSelected]![index]!
-                        Text("\(cardAmount) cards").tag(cardAmount)
+        ScrollView(.vertical, showsIndicators: false){
+            VStack {
+                Group{
+                    Text("Memory Game")
+                    
+                    Spacer()
+                    
+                    Text("Elegí el modo de juego")
+                        .fontWeight(.bold)
+                        .font(.system(.title))
+                        .padding()
+                        .customTextStyle()
+                    
+                    Picker("Modo de Juego", selection: $state.gameModeSelected) {
+                        Text("\(GameModeMemoryGame.classicMultiPlayer.rawValue)").tag(GameModeMemoryGame.classicMultiPlayer)
+                        Text("\(GameModeMemoryGame.classicSinglePlayer.rawValue)").tag(GameModeMemoryGame.classicSinglePlayer)
+                        Text("\(GameModeMemoryGame.sequential.rawValue)").tag(GameModeMemoryGame.sequential)
                     }
-                }
                     .pickerStyle(SegmentedPickerStyle())
                     .padding()
-                
-                Spacer()
+                    
+                    Spacer()
+                    
+                    Text("Cantidad de cards")
+                        .fontWeight(.bold)
+                        .font(.system(.title))
+                        .padding()
+                        .customTextStyle()
+                    
+                    Picker("Cards amount", selection: $state.cardsAmountSelected) {
+                        ForEach((1...CardAmountOptions.count), id: \.self) {index in
+                            let cardAmount = CardAmountOptions[state.gameModeSelected]![index]!
+                            Text("\(cardAmount) cards").tag(cardAmount)
+                        }
+                    }
+                    .pickerStyle(SegmentedPickerStyle())
+                    .padding()
+                    
+                    Spacer()
+                }
+                Group{
+                    Text("Tiempo de previsualizacion")
+                        .fontWeight(.bold)
+                        .font(.system(.title))
+                        .padding()
+                        .customTextStyle()
+                    
+                    Picker("Tiempo de previsualizacion", selection: $state.visualizationTimeSelected) {
+                        Text("3 second").tag(3)
+                        Text("5 seconds").tag(5)
+                        Text("8 seconds").tag(8)
+                    }
+                    .pickerStyle(SegmentedPickerStyle())
+                    .padding()
+                    
+                    Spacer()
+                    Text("Cantidad de vidas")
+                        .fontWeight(.bold)
+                        .font(.system(.title))
+                        .padding()
+                        .customTextStyle()
+                    Picker("Cantidad de vidas", selection: $state.livesAmountSelected) {
+                        Text("2").tag(2)
+                        Text("4").tag(4)
+                        Text("6").tag(6)
+                    }
+                    .pickerStyle(SegmentedPickerStyle())
+                    .padding()
+                    
+                    NavigationLink(destination: MemoryMainView().environmentObject(state)) {
+                        Text("Comenzar juego")
+                            .customButtonStyle()
+                    }.disabled(state.cardsAmountSelected == 0)
+                }
             }
-            Group{
-                Text("Tiempo de previsualizacion")
-                    .fontWeight(.bold)
-                    .font(.system(.title))
-                    .padding()
-                    .customTextStyle()
-                
-                Picker("Tiempo de previsualizacion", selection: $state.visualizationTimeSelected) {
-                    Text("3 second").tag(3)
-                    Text("5 seconds").tag(5)
-                    Text("8 seconds").tag(8)
-                }
-                    .pickerStyle(SegmentedPickerStyle())
-                    .padding()
-                
-                Spacer()
-                Text("Cantidad de vidas")
-                    .fontWeight(.bold)
-                    .font(.system(.title))
-                    .padding()
-                    .customTextStyle()
-                Picker("Cantidad de vidas", selection: $state.livesAmountSelected) {
-                    Text("2").tag(2)
-                    Text("4").tag(4)
-                    Text("6").tag(6)
-                }
-                    .pickerStyle(SegmentedPickerStyle())
-                    .padding()
-                
-                NavigationLink(destination: MemoryMainView().environmentObject(state)) {
-                    Text("Comenzar juego")
-                        .customButtonStyle()
-                }
-            }
-        }
+            
+        }.onChange(of: state.gameModeSelected) { _ in
+            state.cardsAmountSelected = 0        }
     }
 }
